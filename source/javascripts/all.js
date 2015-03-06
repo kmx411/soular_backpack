83415166eb639c3630e02a767c5f71fd537aca9e
Khallil Mangalji <kmx411@gmail.com>
2015-03-07 00:47:58 +0800

    removed slider for input

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,19 +1,18 @@
 //= require_tree .
 $(document).ready( function() {
 
-  $('#donate-slider').slider({
-    value: 1,
-    min: 1,
-    formater: function(value){
-      return '$' + value * 20 + ' // ' + value + ' Backpacks';
-    }
-  });
-
   $('button#top-donate').click( function() {
     var height = $(document).height()
     $('html, body').animate({ 
       scrollTop: height
     }, '1000');
+  });
+
+  $('button#donate-final').click( function() {
+    val = parseInt($('.donate-amount').val(), 10);
+    if( isNaN(val) ) {
+      alert('you cant donate a word :(');
+    }
   });
 
 });
