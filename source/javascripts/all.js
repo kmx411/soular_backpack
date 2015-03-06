2036dd3683595c0ceeb1a0b1cfc0cfcefc417fd0
Khallil Mangalji <kmx411@gmail.com>
2015-03-07 00:16:08 +0800

    missing payment implementation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,19 @@
 //= require_tree .
+$(document).ready( function() {
+
+  $('#donate-slider').slider({
+    value: 1,
+    min: 1,
+    formater: function(value){
+      return '$' + value * 20 + ' // ' + value + ' Backpacks';
+    }
+  });
+
+  $('button#top-donate').click( function() {
+    var height = $(document).height()
+    $('html, body').animate({ 
+      scrollTop: height
+    }, '1000');
+  });
+
+});
