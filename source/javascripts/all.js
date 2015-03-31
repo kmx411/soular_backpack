64e0822b0246673f31811cb0f7f78a13ee5dfa49
Khallil Mangalji <kmx411@gmail.com>
2015-03-31 23:18:13 +0800

    testing stripe

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -8,11 +8,29 @@ $(document).ready( function() {
     }, '1000');
   });
 
-  $('button#donate-final').click( function() {
-    val = parseInt($('.donate-amount').val(), 10);
-    if( isNaN(val) ) {
-      alert('you cant donate a word :(');
+  var handler = StripeCheckout.configure({
+    key: 'pk_live_rbhzYGBFKsQdIq6MVQpe6gjI',
+    image: '/img/documentation/checkout/marketplace.png',
+    token: function(token) {
+      // Use the token to create the charge with a server-side script.
+      // You can access the token ID with `token.id`
     }
+  });
+
+  $('button#donate-final').on('click', function(e) {
+    // Open Checkout with further options
+    handler.open({
+      name: 'Thesoularbackpack',
+      description: '2 widgets',
+      currency: "cad",
+      amount: 1
+    });
+    e.preventDefault();
+  });
+
+  // Close Checkout on page navigation
+  $(window).on('popstate', function() {
+    handler.close();
   });
 
 });
